be5520d7a99e137e4106cb668e473911c6dc389c
Ian Lohan <102693640+Ian-Lohan@users.noreply.github.com>
2023-06-15 01:59:41 -0300

    Add files via upload

--- dblojaderoupas.sql
+++ dblojaderoupas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 13/06/2023 às 19:56
+-- Tempo de geração: 15/06/2023 às 06:59
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -53,7 +53,8 @@ INSERT INTO `tbl_cliente` (`pk_id_cliente`, `cli_nome`, `cli_endereco`, `cli_bai
 (12, 'Jailson Mendes', 'Rua do Oco', 'Ai que delicia', 'Deliciaville', 'RO', 'laranja', 'peça'),
 (13, 'Ben 10', 'Trailer do Vô dele', 'Estrada', 'alguma ai', 'MA', 'alien', 'o curso'),
 (14, 'Irineu', 'Você não sabe nem eu', 'Eu não sei', 'Você não sabe nem eu', 'AC', 'brabo', 'Você não sabe'),
-(16, 'orivaldo', 'pereira', 'da', 'silva', 'AC', '11111-111', '11111111111');
+(16, 'orivaldo', 'pereira', 'da', 'silva', 'AC', '11111-111', '11111111111'),
+(17, 'UniFavip', 'Av. Adjar da Silva Casé', 'Indianopolis', 'Caruaru', 'PE', '55024-740', '3003-4430');
 
 -- --------------------------------------------------------
 
@@ -73,21 +74,21 @@ CREATE TABLE `tbl_produto` (
 --
 
 INSERT INTO `tbl_produto` (`pk_id_produto`, `pro_nome`, `pro_valor`, `pro_estoque`) VALUES
-(1, 'camisa vermelha', 20, 7),
-(3, 'camisa amarela', 20, 5),
-(4, 'camisa azul', 20, 6),
-(6, 'camisa preta', 20, 1),
-(9, 'camisa rosa', 20, 17),
-(10, 'calça jeans', 40, 10),
-(11, 'tenis nike preto', 100, 38),
+(1, 'camisa vermelha', 20, 0),
+(3, 'camisa amarela', 20, 0),
+(4, 'camisa azul', 20, 0),
+(6, 'camisa preta', 20, 0),
+(9, 'camisa rosa', 20, 0),
+(10, 'calça jeans', 40, 0),
+(11, 'tenis nike preto', 100, 0),
 (12, 'chinelo rosa pink havainas', 30, 2),
-(13, 'tenis nike azul', 100, 5),
-(14, 'camisa roxa', 20.5, 5),
-(16, 'bolsa homem-aranha', 25, 3),
+(13, 'tenis nike azul', 100, 0),
+(14, 'camisa roxa', 20.5, 0),
+(16, 'bolsa homem-aranha', 25, 0),
 (17, 'camisa transparente', 0.99, 3),
 (18, 'omnitrix', 1000000, 0),
-(19, 'calcinha masculina', 9.9, 52),
-(20, 'calça formal preta', 50.5, 4),
+(19, 'calcinha masculina', 9.9, 0),
+(20, 'calça formal preta', 50.5, 0),
 (21, 'camisa ednaldo pereira', 5.5, 2),
 (22, 'camisa manoel gomes', 5.5, 5);
 
@@ -115,7 +116,7 @@ INSERT INTO `tbl_usuario` (`pk_id_usuario`, `usu_nome`, `usu_login`, `usu_senha`
 (4, 'Diabo', 'devil', 'hell'),
 (5, 'Goku', 'oi_eu_sou_o_goku', 'kamehameha'),
 (6, 'Naruto', 'kurama', 'rasengan'),
-(7, 'Luffy', 'joyboy', 'gomugomunopassword'),
+(7, 'Luffy', 'strawhat', 'gomugomunopassword'),
 (8, 'Ichigo', 'hollow', 'BANKAI!'),
 (9, 'Yusuke', 'hakusho', 'spirit_gun'),
 (10, 'Yugi', 'horadoduelo', 'exodia'),
@@ -178,7 +179,22 @@ INSERT INTO `tbl_vendas` (`pk_id_vendas`, `fk_cliente`, `ven_data_venda`, `ven_v
 (35, 1, '2023-06-13', 50.5, 60.5, 10),
 (36, 5, '2023-06-13', 27.5, 27.5, 0),
 (37, 1, '2023-06-13', 62, 62, 0),
-(38, 9, '2023-06-13', 10, 11, 1);
+(38, 9, '2023-06-13', 10, 11, 1),
+(39, 11, '2023-06-13', 160.5, 190.5, 30),
+(40, 1, '2023-06-13', 0, 0, 0),
+(41, 1, '2023-06-14', 80, 80, 0),
+(42, 1, '2023-06-14', 80, 80, 50),
+(43, 1, '2023-06-14', 80, 80, 50),
+(44, 1, '2023-06-14', 165, 165, 50),
+(45, 1, '2023-06-14', 81, 81, 20),
+(46, 1, '2023-06-14', 81, 81, 20),
+(47, 1, '2023-06-14', 140, 140, 40),
+(48, 1, '2023-06-14', 140, 140, 40),
+(49, 1, '2023-06-14', 40, 40, 20),
+(50, 1, '2023-06-14', 40, 40, 20),
+(51, 1, '2023-06-14', 40, 40, 20),
+(52, 1, '2023-06-14', 100, 140, 140),
+(53, 1, '2023-06-14', 19, 19.8, 19.799999237060547);
 
 -- --------------------------------------------------------
 
@@ -211,7 +227,60 @@ INSERT INTO `tbl_vendas_produtos` (`pk_id_venda_produto`, `fk_produto`, `fk_vend
 (31, 9, 37, 20, 2),
 (32, 22, 36, 5.5, 3),
 (33, 21, 36, 5.5, 2),
-(35, 22, 38, 5.5, 2);
+(35, 22, 38, 5.5, 2),
+(36, 6, 39, 20, 2),
+(37, 11, 39, 100, 1),
+(38, 20, 39, 50.5, 1),
+(39, 1, 40, 20, 1),
+(40, 3, 40, 20, 1),
+(41, 10, 40, 40, 1),
+(42, 1, 41, 20, 1),
+(43, 1, 41, 20, 1),
+(44, 1, 41, 20, 1),
+(45, 1, 41, 20, 1),
+(46, 10, 42, 40, 1),
+(47, 9, 42, 20, 2),
+(48, 10, 42, 40, 1),
+(49, 9, 42, 20, 2),
+(50, 10, 43, 40, 1),
+(51, 9, 43, 20, 2),
+(52, 10, 44, 40, 1),
+(53, 11, 44, 100, 1),
+(54, 16, 44, 25, 1),
+(55, 14, 45, 20.5, 1),
+(56, 3, 45, 20, 1),
+(57, 3, 45, 20, 1),
+(58, 14, 45, 20.5, 1),
+(59, 14, 45, 20.5, 1),
+(60, 3, 45, 20, 1),
+(61, 3, 45, 20, 1),
+(62, 14, 45, 20.5, 1),
+(63, 14, 46, 20.5, 1),
+(64, 3, 46, 20, 1),
+(65, 3, 46, 20, 1),
+(66, 14, 46, 20.5, 1),
+(67, 13, 47, 100, 1),
+(68, 4, 47, 20, 1),
+(69, 3, 47, 20, 1),
+(70, 13, 47, 100, 1),
+(71, 4, 47, 20, 1),
+(72, 3, 47, 20, 1),
+(73, 13, 48, 100, 1),
+(74, 4, 48, 20, 1),
+(75, 3, 48, 20, 1),
+(76, 4, 49, 20, 1),
+(77, 3, 49, 20, 1),
+(78, 4, 49, 20, 1),
+(79, 3, 49, 20, 1),
+(80, 4, 50, 20, 1),
+(81, 3, 50, 20, 1),
+(82, 4, 51, 20, 1),
+(83, 3, 51, 20, 1),
+(84, 13, 52, 100, 1),
+(85, 3, 52, 20, 1),
+(86, 4, 52, 20, 1),
+(87, 19, 53, 9.9, 1),
+(88, 19, 53, 9.9, 1);
 
 --
 -- Índices para tabelas despejadas
@@ -258,7 +327,7 @@ ALTER TABLE `tbl_vendas_produtos`
 -- AUTO_INCREMENT de tabela `tbl_cliente`
 --
 ALTER TABLE `tbl_cliente`
-  MODIFY `pk_id_cliente` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `pk_id_cliente` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de tabela `tbl_produto`
@@ -276,13 +345,13 @@ ALTER TABLE `tbl_usuario`
 -- AUTO_INCREMENT de tabela `tbl_vendas`
 --
 ALTER TABLE `tbl_vendas`
-  MODIFY `pk_id_vendas` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `pk_id_vendas` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT de tabela `tbl_vendas_produtos`
 --
 ALTER TABLE `tbl_vendas_produtos`
-  MODIFY `pk_id_venda_produto` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `pk_id_venda_produto` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
 
 --
 -- Restrições para tabelas despejadas
